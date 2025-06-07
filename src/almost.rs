42c529258f3e3db2d77e22f127426dd356514d98
Thomas Conway <drtomc@gmail.com>
2025-06-07 14:45:27 +1000

    breakend merging

--- src/almost.rs
+++ src/almost.rs
@@ -21,7 +21,8 @@ pub async fn find_almost_exact(
     tbl: DataFrame,
     n: u32,
 ) -> std::io::Result<DataFrame> {
-    find_almost_exact_non_bnd(ctx, tbl, n).await
+    let tbl = find_almost_exact_non_bnd(ctx, tbl, n).await?;
+    find_almost_exact_bnd(ctx, tbl, n).await
 }
 
 pub async fn find_almost_exact_non_bnd(
@@ -122,6 +123,126 @@ pub async fn find_almost_exact_non_bnd(
 
     if false {
         resolution.clone().show().await?;
+    }
+
+    if false {
+        tbl.clone()
+            .aggregate(
+                vec![col("row_id")],
+                vec![count(col("row_key")).alias("count")],
+            )?
+            .filter(col("count").gt(lit(1)))?
+            .sort_by(vec![col("row_id")])?
+            .show()
+            .await?;
+    }
+
+    let tbl = tbl
+        .join(
+            resolution,
+            datafusion::common::JoinType::Left,
+            &["row_id"],
+            &["orig_row_id"],
+            None,
+        )?
+        .with_column(
+            "row_key",
+            coalesce(vec![col("new_row_key"), col("row_key")]),
+        )?
+        .with_column(
+            "vix_count",
+            coalesce(vec![col("new_vix_count"), col("vix_count")]),
+        )?
+        .with_column(
+            "vix_set",
+            coalesce(vec![col("new_vix_set"), col("vix_set")]),
+        )?
+        .drop_columns(&["orig_row_id", "new_row_key", "new_vix_count", "new_vix_set"])?;
+
+    if false {
+        tbl.clone()
+            .sort_by(vec![
+                col("chrom_id"),
+                col("start"),
+                col("end"),
+                col("row_id"),
+            ])?
+            .show()
+            .await?;
+    }
+
+    Ok(tbl)
+}
+
+pub async fn find_almost_exact_bnd(
+    ctx: &SessionContext,
+    tbl: DataFrame,
+    n: u32,
+) -> std::io::Result<DataFrame> {
+    let lhs = tbl
+        .clone()
+        .filter(col("vix_count").lt(lit(n)).and(col("kind").eq(lit("BND"))))?
+        .select(vec![
+            col("row_id").alias("lhs_row_id"),
+            col("chrom_id").alias("lhs_chrom_id"),
+            col("start").alias("lhs_start"),
+            col("end").alias("lhs_end"),
+            col("chrom2_id").alias("lhs_chrom2_id"),
+            col("end2").alias("lhs_end2"),
+            col("row_key").alias("lhs_row_key"),
+            col("vix_count").alias("lhs_vix_count"),
+            col("vix_set").alias("lhs_vix_set"),
+        ])?;
+    let rhs = tbl
+        .clone()
+        .filter(col("vix_count").lt(lit(n)).and(col("kind").eq(lit("BND"))))?
+        .select(vec![
+            col("row_id").alias("rhs_row_id"),
+            col("chrom_id").alias("rhs_chrom_id"),
+            col("start").alias("rhs_start"),
+            col("end").alias("rhs_end"),
+            col("chrom2_id").alias("rhs_chrom2_id"),
+            col("end2").alias("rhs_end2"),
+            col("row_key").alias("rhs_row_key"),
+            col("vix_count").alias("rhs_vix_count"),
+            col("vix_set").alias("rhs_vix_set"),
+        ])?;
+
+    //lhs.clone().show().await?;
+
+    let almost_exact = lhs
+        .join(
+            rhs,
+            datafusion::common::JoinType::Inner,
+            &["lhs_chrom_id", "lhs_chrom2_id"],
+            &["rhs_chrom_id", "rhs_chrom2_id"],
+            Some(
+                lit(true)
+                    .and(col("lhs_row_id").lt(col("rhs_row_id")))
+                    .and(abs(col("lhs_start") - col("rhs_start")).lt(lit(25)))
+                    .and(abs(col("lhs_end") - col("rhs_end")).lt(lit(25)))
+                    .and((col("lhs_vix_set") & col("rhs_vix_set")).eq(lit(0))),
+            ),
+        )?
+        .sort(vec![
+            (col("lhs_vix_count") + col("rhs_vix_count")).sort(false, false),
+            col("lhs_row_key").sort(true, false),
+            col("rhs_row_key").sort(true, false),
+        ])?;
+
+    if false {
+        almost_exact.clone().show().await?;
+    }
+
+    let batch: Vec<RecordBatch> = almost_exact.collect().await?;
+
+    let resolution = resolve_groups(batch).await?;
+    let resolution = ctx
+        .read_batch(resolution)
+        .map_err(|e| Error::new(ErrorKind::Other, e))?;
+
+    if true {
+        resolution.clone().sort_by(vec![col("new_row_key")])?.show().await?;
     }
 
     if false {
