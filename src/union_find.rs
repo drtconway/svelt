4621fcfa7b4663f5599e12d09bbba1e3565f945b
Tom Conway <drtconway@users.noreply.github.com>
2025-06-24 16:10:57 +1000

    add some testing

--- src/union_find.rs
+++ src/union_find.rs
@@ -60,7 +60,7 @@ impl UnionFind {
             }
 
             let old = ((lhs_rank as u64) << 32) | (lhs_id as u64);
-            let new = ((lhs_rank as u64) << 32) | (lhs_id as u64);
+            let new = ((lhs_rank as u64) << 32) | (rhs_id as u64);
 
             if let Err(_) = self.data[lhs_id as usize].compare_exchange(
                 old,
@@ -94,5 +94,51 @@ impl UnionFind {
 
     fn rank(&self, id: u32) -> u32 {
         (self.data[id as usize].load(Ordering::Relaxed) >> 32 & 0xFFFFFFFF) as u32
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use rand::{SeedableRng, rngs::StdRng, seq::SliceRandom};
+
+    use crate::disjoint_set::DisjointSet;
+
+    use super::*;
+
+    #[test]
+    fn basic_union_find() {
+        let s = 19;
+        let mut rng = StdRng::seed_from_u64(s);
+        let n = 100;
+        let mut xs: Vec<u32> = (0..n).collect();
+        xs.shuffle(&mut rng);
+        let pairs1: Vec<(u32, u32)> = xs.chunks(2).map(|c| (c[0], c[1])).collect();
+
+        let mut ds = DisjointSet::new();
+        let uf = UnionFind::new(n as usize);
+
+        for (x, y) in pairs1.iter() {
+            let x = *x;
+            let y = *y;
+
+            assert_eq!(uf.find(x), x);
+            assert_eq!(uf.find(y), y);
+        }
+
+        for (x, y) in pairs1.iter() {
+            let x = *x;
+            let y = *y;
+
+            ds.union(x, y);
+            uf.union(x, y);
+        }
+
+        for (x, y) in pairs1.iter() {
+            let x = *x;
+            let y = *y;
+
+            assert_eq!(ds.find(x), ds.find(y));
+            assert_eq!(uf.find(x), uf.find(y));
+        }
     }
 }
